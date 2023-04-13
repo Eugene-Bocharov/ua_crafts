9bbfcab9f19504bc930a0aa2f1e1434bc296207b
bilingual-vovs <vovavovsart@gmail.com>
2023-04-13 20:29:42 +0300

    Filters

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './Filters.module.css';
+import checkboxStyle from './Checkbox.module.css';
 
 import filter from './Filter.svg';
+import arrow from './Arrow.svg';
 
 interface filterProps {
   children: string;
@@ -10,8 +12,13 @@ interface filterProps {
 const FilterItem: React.FC<filterProps> = ({ children }) => {
   return (
     <React.Fragment>
-      <li>{children}</li>
-      <input type={'checkbox'} />
+      <li className={styles['filter-li']}>
+        {children}
+        <input
+          className={checkboxStyle['control-indicator']}
+          type={'checkbox'}
+        />
+      </li>
     </React.Fragment>
   );
 };
@@ -20,18 +27,22 @@ export const Filters: React.FC = () => {
   return (
     <React.Fragment>
       <div className={styles.main}>
-        <h1>
-          Filters
+        <div className={styles.flex}>
+          <h1 className={styles.heading}>Filters</h1>
           <img src={filter} alt="filter.ico" />
-        </h1>
-        <h2>Product type</h2>
-        <ul>
-          <FilterItem> Embroidered </FilterItem>
-          <li>Embroidered Shirt</li>
-          <li>Keychains</li>
-          <li>Bracelets</li>
-          <li>Necklaces</li>
+        </div>
+        <h2 className={styles.section}>Product type</h2>
+        <ul className={styles.ul}>
+          <FilterItem>Knitted products</FilterItem>
+          <FilterItem>Embroidered Shirt</FilterItem>
+          <FilterItem>Keychains</FilterItem>
+          <FilterItem>Bracelets</FilterItem>
+          <FilterItem>Necklaces</FilterItem>
         </ul>
+        <button className={`${styles.flex} ${styles.invise} ${styles.more}`}>
+          <p>View more filters</p>
+          <img className={styles.arrow} src={arrow} alt="" />
+        </button>
       </div>
     </React.Fragment>
   );
